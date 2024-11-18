d2d4f3fc764a39ccf5b12e4fad906c9edbe55c68
jainmitesh2393 <115924659+jainmitesh2393@users.noreply.github.com>
2024-11-18 22:05:22 +0530

    Update final.py

--- final.py
+++ final.py
@@ -15,7 +15,7 @@ from openpyxl.styles import Font, Alignment, PatternFill
 
 # Initialize the recognizer and text-to-speech engine
 recognizer = sr.Recognizer()
-engine = pyttsx3.init(driverName='espeak-ng')
+engine = pyttsx3.init(driverName='espeak')
 
 # Function to speak text
 def speak(text):
